5dc345c8f1f8d5be3e605f3da25f90826173cec6
Pawandeep Singh <pawandeepss@u.nus.edu>
2024-05-26 15:41:40 +0800

    Update middleware handler

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -6,7 +6,39 @@ import io from 'socket.io-client';
 const socket = io('http://localhost:3000');
 
 export async function GET() {
+  console.log("GET API CCALLED")
   
+  const BOT_TOKEN = "7174167825:AAHJ4TergSi7IFkZRkeHJx1_Iw9mMr4Jztc";
+  const CHAT_ID = "@ocp_testt";
+  const MESSAGE = "Hello, appointment is sent!";
+
+  // Telegram API endpoint
+  const TELEGRAM_API_URL = `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`;
+
+  // Data to be sent in the POST request
+  const requestBody = {
+    chat_id: CHAT_ID,
+    text: MESSAGE,
+  };
+
+  // Options for the fetch request
+  const requestOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(requestBody),
+  };
+
+  // Sending the message
+  await fetch(TELEGRAM_API_URL, requestOptions)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Message sent successfully:", data);
+    })
+    .catch((error) => {
+      console.error("Error sending message:", error);
+    });
 
   return Response.json({ test: "hello world" });
 }
@@ -19,42 +51,6 @@ export async function POST() {
       // (like database operations, etc.)
       console.log("post called")
       socket.emit('appointment', 'send appointment to client, it works!');
-
-
-      const BOT_TOKEN = "7174167825:AAHJ4TergSi7IFkZRkeHJx1_Iw9mMr4Jztc";
-      const CHAT_ID = "@ocp_testt";
-      const MESSAGE = "Hello, appointment is sent!";
-    
-      // Telegram API endpoint
-      const TELEGRAM_API_URL = `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`;
-    
-      // Data to be sent in the POST request
-      const requestBody = {
-        chat_id: CHAT_ID,
-        text: MESSAGE,
-      };
-    
-      // Options for the fetch request
-      const requestOptions = {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(requestBody),
-      };
-    
-      // Sending the message
-      await fetch(TELEGRAM_API_URL, requestOptions)
-        .then((response) => response.json())
-        .then((data) => {
-          console.log("Message sent successfully:", data);
-        })
-        .catch((error) => {
-          console.error("Error sending message:", error);
-        });
-
-
-      
 
       return NextResponse.json({ data: 'Success' }, { status: 200 });
 
